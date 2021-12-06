ae4ec82c5bfe0c143df02515432bc7cf7c610c1c
Yuri <projetohavoc@gmail.com>
2021-12-06 18:26:08 -0300

    registro

--- registro_hora_extra/models.py
+++ registro_hora_extra/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from funcionarios.models import Funcionario
 
 # Create your models here.
 
@@ -6,6 +7,7 @@ from django.db import models
 class RegistroHoraExtra(models.Model):
     
     motivo = models.CharField(max_length=100)
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.motivo
