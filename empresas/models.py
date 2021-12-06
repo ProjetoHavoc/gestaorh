b6ebdb3eae5378f7c4c7c33d8a5af74d971ae530
Yuri <projetohavoc@gmail.com>
2021-12-06 14:55:50 -0300

    funcionarios

--- empresas/models.py
+++ empresas/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class Empresa(models.Model):
 
     nome = models.CharField(max_length=100, help_text='nome da empresa')
+
+    def __str__(self):
+    
+        return self.nome  
