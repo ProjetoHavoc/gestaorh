1b716417c80f76738d3d0025b0937f44020b2ff0
ProjetoHavoc <92698648+ProjetoHavoc@users.noreply.github.com>
2021-12-12 16:36:05 -0300

    auth

---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from funcionarios.models import Funcionario
+
+# Create your views here.
+
+@login_required
+def home(request):
+  
+    data = {}
+    data['usuario'] = request.user
+    
+    return render(request, 'core/index.html', data)
--- gestaorh/settings.py
+++ gestaorh/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
     'empresas',
     'funcionarios',
     'departamentos',
@@ -59,7 +61,7 @@ ROOT_URLCONF = 'gestaorh.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -123,6 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),    
+]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
